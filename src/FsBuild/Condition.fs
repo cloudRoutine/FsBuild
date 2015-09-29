d8d786448d143ae92c3f260a38d9aa0ec69b93f4
Jared Hester <jared.hester@gmail.com>
2015-09-29 00:30:55 -0400

    rough start
    some reference code
    some code written earlier
    added some packages

---
+++ src/FsBuild/Condition.fs
@@ -0,0 +1,131 @@
+﻿namespace Templater
+open System
+open System.Xml
+open System.Xml.Linq
+open Microsoft.Build
+open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Quotations.Patterns
+open Microsoft.FSharp.Quotations.DerivedPatterns
+open Microsoft.FSharp.Reflection
+open System.Reflection
+open System.Text
+// MSBUILD project file schema reference
+// https://msdn.microsoft.com/en-us/library/5dy88c2e.aspx
+
+
+
+// VSIX Extension Schema 2.0 Reference 
+// https://msdn.microsoft.com/en-us/library/hh696828.aspx
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Condition =
+
+// MSBuild Conditions
+// https://msdn.microsoft.com/en-us/library/7szfhaft.aspx
+
+    let squote (str:string) =
+        String.Concat["'";str;"'"]
+
+    let condstr expr =
+        let sb = StringBuilder()
+        let inline append (s:^a) = sb.Append s |> ignore
+        let inline appendstr x   = string x |> append
+
+        let rec loop expr =
+            let inline loopls ls  = List.iter loop ls
+
+            /// If necessary surround the expression with parentheses
+            let inline paren ex   = 
+                match ex with
+                | Value(vl,_)               ->  appendstr vl
+                | SpecificCall <@ not @> 
+                    (_,_,[ Value(vl,_)] )   ->  append "!";appendstr vl
+                | SpecificCall <@ not @> 
+                    (_,_, SpecificCall<@(=)@>
+                            (_,_,expls)::[])  
+                                            ->  loop ex
+                | _                         ->  append "("; loop ex; append ")"
+
+            let inline parenls ls = append "("; loopls ls; append ")"
+
+            match expr with
+            | Value( value,_)           ->  appendstr value
+            | SpecificCall <@ not @> 
+                (_,_, [x])              ->  append "!"; paren x
+            | SpecificCall <@ not @> 
+                (_,_, exprls)           ->  append "!"; parenls exprls 
+            | SpecificCall <@(=)@> 
+                (_, _,lexp::[rexp])     ->  paren lexp; append " == "; paren rexp
+            | SpecificCall <@(=)@> 
+                (_, _, hd::tl)          ->  paren hd; append " == "; loopls tl
+            | SpecificCall <@(>)@>
+                (_, _, hd::tl)          ->  paren hd; append " &gt; "; loopls tl
+            | SpecificCall <@(<)@>
+                (_, _, hd::tl)          ->  paren hd; append " &lt; "; loopls tl
+            | SpecificCall <@(>=)@>
+                (_, _, hd::tl)          ->  paren hd; append " &gt;= "; loopls tl
+            | SpecificCall <@(<=)@>
+                (_, _, hd::tl)          ->  paren hd; append " &lt;= "; loopls tl
+            | _                         ->  string expr |> append 
+        loop expr
+        string sb
+
+    type Condition (expr: Expr<bool>) =
+        member __.Expr = expr
+            
+    let (.<>.) (c1:string)  (c2:string)  =  Condition(<@ c1 <> c2 @>)
+    let (.>=.) (c1:decimal) (c2:decimal) =  Condition(<@ c1 >= c2 @>)
+    let (.<=.) (c1:decimal) (c2:decimal) =  Condition(<@ c1 <= c2 @>)
+   
+  
+
+
+
+
+
+
+
+//        static member inline (==) (c1:Condition, c2:Condition) =
+//            Condition(<@ %c1.Expr = %c2.Expr @>)
+//
+//        static member inline (!=) (c1:Condition, c2:Condition) =
+//            Condition(<@ %c1.Expr <> %c2.Expr @>)
+//
+//        static member op_Explicit (cond:Condition) : string =
+//            condstr cond.Expr
+//
+//        override self.ToString() =
+//           condstr self.Expr 
+
+
+    let (.=.)  (c1:string) (c2:string) =  
+        let c1', c2' = squote c1, squote c2
+        Condition(<@ c1' = c2' @>)
+
+
+
+    let (.||.) (c1:Condition)(c2:Condition) =
+            Condition(<@ %c1.Expr || %c2.Expr @>)
+
+    let (.&&.) (c1:Condition)(c2:Condition) =
+            Condition(<@ %c1.Expr && %c2.Expr @>)
+
+
+    let makecondition (combo:'a->'a->Condition) (arg1:'a) (arg2:'a) =
+        combo arg1 arg2
+
+    let z = makecondition (.<>.) "one" "two"
+    let z1 = makecondition (.>=.) 23.0m  233.m
+    let z2 = makecondition (.||.)   z z1
+
+
+//    type ConditionBuilder () =
+//        let data = xattr "Condition" []
+//        member __.Zero () = data
+//
+//    let cond expr = Condition expr
+//        
+//    let x = ((cond <@ 2 = 3 @>) !=  (cond <@ not (10 <> 4) @>) ) |> string
+
+
+
+
---
+++ src/FsBuild/FsBuild.fs
@@ -0,0 +1,92 @@
+﻿module FsBuild
+
+open System.Xml.Linq
+
+(*
+Under the hood, attrs and elems are just .Net XElements and XAttributes,
+in the MSBuild XML namespace.
+*)
+let nsName = "http://schemas.microsoft.com/developer/msbuild/2003"
+let ns = XNamespace.op_Implicit(nsName)
+
+let elem
+  (name: string)
+  (attrs: list<XAttribute>)
+  (children: list<XNode>): XElement =
+  let _elem = new XElement(ns + name)
+
+  attrs |> List.iter (fun attr -> _elem.Add(attr))
+  children |> List.iter (fun child -> _elem.Add(child))
+  _elem
+
+let attr (name: string) (value: string): XAttribute =
+  new XAttribute(XName.op_Implicit(name), value)
+
+(*
+All supported attributes.
+*)
+module A =
+  let Condition        = attr "Condition"
+  let DefaultTargets   = attr "DefaultTargets"
+  let DependsOnTargets = attr "DependsOnTargets"
+  let Directories      = attr "Directories"
+  let Files            = attr "Files"
+  let Include          = attr "Include"
+  let Inputs           = attr "Inputs"
+  let Name             = attr "Name"
+  let OutputAssembly   = attr "OutputAssembly"
+  let Outputs          = attr "Outputs"
+  let Platform         = attr "Platform"
+  let Project          = attr "Project"
+  let References       = attr "References"
+  let Returns          = attr "Returns"
+  let Sources          = attr "Sources"
+  let TargetType       = attr "TargetType"
+  let ToolsVersion     = attr "ToolsVersion"
+
+(*
+All supported elements.
+*)
+let Choose                  = elem "Choose"
+let Csc                     = elem "Csc"
+let Delete                  = elem "Delete"
+let Fsc                     = elem "Fsc"
+let Import                  = elem "Import"
+let Item                    = elem
+let ItemGroup               = elem "ItemGroup"
+let MakeDir                 = elem "MakeDir"
+let Otherwise               = elem "Otherwise"
+let Property                = elem
+let PropertyGroup           = elem "PropertyGroup"
+let RemoveDir               = elem "RemoveDir"
+let Target                  = elem "Target"
+let Text (s: string): XText = new XText(s)
+let When                    = elem "When"
+
+let ProjectObj attrs children =
+  let fsTargets30 =
+    @"$(MSBuildExtensionsPath32)\..\Microsoft SDKs\F#\3.0\Framework\v4.0\Microsoft.FSharp.targets"
+  let fsTargets31 =
+    @"$(MSBuildExtensionsPath32)\..\Microsoft SDKs\F#\3.1\Framework\v4.0\Microsoft.FSharp.targets"
+
+  let _attrs = attr "xmlns" nsName :: attrs
+  let _children =
+    Choose
+      []
+      [ When
+          [ A.Condition <| sprintf "Exists('%s')" fsTargets31 ]
+          [ PropertyGroup
+              []
+              [ Property "FSTargetsPath" [] [ Text fsTargets31 ] ] ]
+        When
+          [ A.Condition <| sprintf "Exists('%s')" fsTargets30 ]
+          [ PropertyGroup
+              []
+              [ Property "FSTargetsPath" [] [ Text fsTargets30 ] ] ] ] ::
+    Import [ A.Project "$(FSTargetsPath)" ] [] ::
+    children
+  new XElement(ns + "Project", _attrs, _children)
+
+let Project attrs children =
+  (ProjectObj attrs children).Save("Build.xml")
+

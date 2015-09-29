46983c487ab124d30651c7503591a9bc800b827b
Jared Hester <jared.hester@gmail.com>
2015-09-29 00:16:56 -0400

    Setup Project Scaffold

---
+++ src/FsBuild/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FsBuild")>]
+[<assembly: AssemblyProductAttribute("FsBuild")>]
+[<assembly: AssemblyDescriptionAttribute("EDSL for .fsproj files and MSBuild Tasks")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ tests/FsBuild.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module FsBuild.Tests
+
+open FsBuild
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
